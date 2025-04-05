a570cd8654e89a57032b4880c336efd9eef5487e
EdenWijaya <eden.122140187@student.itera.ac.id>
2025-04-06 01:43:49 +0700

    add product api

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import IsiProduct from '../components/IsiProduct'
+import Footer from '../components/Footer'
 
 const Product = () => {
     return (
         <>
             <Navbar />
+            <IsiProduct />
+            <Footer />
         </>
   )
 }
